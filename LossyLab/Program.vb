cd746978e26a25e1c47eab199b89bc79630e205a
Asher-1971 <aliasher.0834@bvsparsischool.edu.pk>
2024-01-15 23:24:20 +0500

    reset module and file handlin

--- LossyLab/Program.vb
+++ LossyLab/Program.vb
@@ -1,13 +1,29 @@
 Imports System
+Imports System.IO
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("did this on vs code")
-        Console.WriteLine("")
-        Console.WriteLine("did this on vs code")
-        Console.WriteLine("did this on visual stuidio desktop ")
+        ' Set the default file name
+        Dim fileName As String = "userInput.txt"
+        Dim userInput As String = ""
+        Dim line As String
 
+        Console.WriteLine("Enter text (type 'END' to finish):")
 
+        ' Read lines from the console until the user types 'END'
+        Do
+            line = Console.ReadLine()
+            If line <> "END" Then
+                userInput &= line & Environment.NewLine
+            End If
+        Loop Until line = "END"
+
+        ' Write the user input to a file
+        File.WriteAllText(fileName, userInput)
+
+        ' Read the file and display its contents
+        Dim fileContents As String = File.ReadAllText(fileName)
+        Console.WriteLine("File contents:")
+        Console.WriteLine(fileContents)
     End Sub
 End Module
