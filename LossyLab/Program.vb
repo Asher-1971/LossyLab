b28a44c3093ea30f0b97781b181cc727db823f62
Asher-1971 <aliasher.0834@bvsparsischool.edu.pk>
2024-01-15 23:14:18 +0500

    vs code checkbase system

--- LossyLab/Program.vb
+++ LossyLab/Program.vb
@@ -3,5 +3,9 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+        console.WriteLine("did this on vs code prtable")
+        console.writeline("")
+        console.WriteLine("did this on vs code prtable")
+
     End Sub
 End Module
