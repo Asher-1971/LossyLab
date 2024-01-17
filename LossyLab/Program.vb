f8fc5faecc3ea020bc42ba8fe6d104702f4f4f58
Asher-1971 <aliasher.0834@bvsparsischool.edu.pk>
2024-01-17 23:48:40 +0500

    check

--- LossyLab/Program.vb
+++ LossyLab/Program.vb
@@ -3,27 +3,6 @@ Imports System.IO
 
 Module Program
     Sub Main(args As String())
-        ' Set the default file name
-        Dim fileName As String = "userInput.txt"
-        Dim userInput As String = ""
-        Dim line As String
-
-        Console.WriteLine("Enter text (type 'END' to finish):")
-
-        ' Read lines from the console until the user types 'END'
-        Do
-            line = Console.ReadLine()
-            If line <> "END" Then
-                userInput &= line & Environment.NewLine
-            End If
-        Loop Until line = "END"
-
-        ' Write the user input to a file
-        File.WriteAllText(fileName, userInput)
-
-        ' Read the file and display its contents
-        Dim fileContents As String = File.ReadAllText(fileName)
-        Console.WriteLine("File contents:")
-        Console.WriteLine(fileContents)
+       console.writeline("heello ")
     End Sub
 End Module
